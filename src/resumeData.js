1d44eec6d1c5184d62694780a58181d913f27091
krystal <krystal.rodriguez6@lc.cuny.edu>
2022-05-24 21:16:10 -0400

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://krystalxx2.github.io/",
+    "name": "Krystal Rodriguez",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"krystalxx2",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to explore in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/krystalxx2/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/krystalxx2",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/krystalxx2",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at Lehman College and pursuing my Bachelors Degree. I am a self taught Full Stack Web Developer, currently diving deeper into CGI. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"New York",
+    "website":"https://krystalxx2.github.io",
+    "education":[
+      {
+        "UniversityName":"Lehman College",
+        "specialization":"WonderGurl",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Achievements":"A Ton Achievements"
+      },
+      {
+        "UniversityName":"HotGirl University",
+        "specialization":"Too Wavy",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"A Whole lot of Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Disney Pixar",
+        "specialization":"Animation & Architecture",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"Currently working here",
+        "Achievements":"A Whole lot of Achievements"
+      },
+      {
+        "CompanyName":"Nickelodeon",
+        "specialization":"Super Talented",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2021",
+        "Achievements":"A Whole lot of Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"cuteness1",
+        "imgurl":"images/p.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"cuteness2",
+        "imgurl":"images/s.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"cuteness3",  
+        "imgurl":"images/r.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"cuteness4",
+        "imgurl":"images/k.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"In order to fly, first you must run",
+        "name":"Some technical gal"
+      },
+      {
+        "description":"In order to fly, first you must run",
+        "name":"Some technical gal"
+      }
+    ]
+  }
+  
+  export default resumeData
